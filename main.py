3ef9fbe6adaf55628ae9bd343b6674e0dd074867
Vicente Zepeda Mas <vzepedam@redhat.com>
2021-02-19 14:40:24 +0100

    Initial commit for collaborator check
    Signed-off-by: Vicente Zepeda Mas <vzepedam@redhat.com>

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
+#!/usr/bin/env python3
+
 import os
-import requests  # noqa We are just importing this to prove the dependency installed correctly
+from github import Github
 
 
-def main():
-    my_input = os.environ["INPUT_MYINPUT"]
+def main(*args):
+    if len(args) != 4:
+        print("Wrong number of arguments")
+        return 1
+    _github_token = args[0]
+    _github_repo = args[1]
+    _github_pr = args[2]
+    _authorized = args[2]
+    repo = Github(_github_token).get_repo(_github_repo)
+    pr = repo.get_pull(_github_pr)
 
-    my_output = f"Hello {my_input}"
+    _username = pr.user.name
 
-    print(f"::set-output name=myOutput::{my_output}")
+    if _username in _authorized:
+        return 0
+
+    print(f"Unauhtorized user: {_username}")
+    return 1
 
 
 if __name__ == "__main__":
